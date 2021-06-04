af202eae9e1132a496b4950c7c59c7b34bcc0e66
liuxfe <1206111783@qq.com>
2021-06-04 13:31:04 +0800

    add main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// Google AdSense
+(function(){
+  var ad = document.createElement("script");
+  ad.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  ad.setAttribute("data-ad-client", "ca-pub-9312750344484857");
+  ad.setAttribute("async", true);
+  document.head.appendChild(ad);
+})();
+
+// Google Analytics
+(function(){
+  var ga = document.createElement("script");
+  ga.src = "https://www.googletagmanager.com/gtag/js?id=UA-195431371-1";
+  ga.setAttribute("async", true);
+  document.head.appendChild(ga);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-195431371-1');
+})();
+
+// baidu tongji
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bc24c40486ac78730f85549d164dac6f";
+  hm.setAttribute("async", true);
+  document.head.appendChild(hm);
+})();
