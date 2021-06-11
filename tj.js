57175a4d8b266ac9b6578d2932030e1d4f0ba697
liuxfe <1206111783@qq.com>
2021-06-11 18:39:59 +0800

    add-tj.js

---
+++ tj.js
@@ -0,0 +1,43 @@
+/*
+// Google AdSense
+(function(){
+  var ad = document.createElement("script");
+  ad.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  ad.setAttribute("data-ad-client", "ca-pub-9312750344484857");
+  ad.setAttribute("async", true);
+  document.head.appendChild(ad);
+})();
+
+// Viglink:1206111783@qq.com
+var vglnk = {key: '1ebfb4d3c899a2a3607eee473b5a5176'};
+(function(d, t) {
+    var s = d.createElement(t);
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//cdn.viglink.com/api/vglnk.js';
+    var r = d.getElementsByTagName(t)[0];
+        r.parentNode.insertBefore(s, r);
+}(document, 'script'));
+*/
+
+// Google Analytics
+(function(){
+  var ga = document.createElement("script");
+  ga.src = "https://www.googletagmanager.com/gtag/js?id=UA-195431371-1";
+  ga.setAttribute("async", true);
+  document.head.appendChild(ga);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-195431371-1');
+})();
+
+// baidu tongji
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?358b1a485f5d2756a663674529013da2";
+  hm.setAttribute("async", true);
+  document.head.appendChild(hm);
+})();
